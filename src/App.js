2a61f1c7b8ac5065557070ca8af43627fbc60ac1
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-16 07:29:14 +0100

    link header to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <rockets />
+        </Route>
+        <Route path="/myProfile">
+          <myprofile />
+        </Route>
+        <Route path="/Dragons">
+          <dragons />
+        </Route>
+        <Route path="/mission">
+          <missions />
+        </Route>
+      </Switch>
+
+    </>
   );
 }
 
