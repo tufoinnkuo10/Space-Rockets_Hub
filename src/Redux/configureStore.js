92088d14dd52a423aba7611d74904f8b40f32cb2
AZARIA SAIDI <35831836+azy64@users.noreply.github.com>
2021-11-17 14:43:26 +0100

    Update configureStore.js

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,12 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import missions from './missions/missions';
 import dragons from './dragons/dragons';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import fetchrocketAPI from '../components/Apirocket';
 import rocketReducer, { getRockets } from './rockets/rockets';
 
-const reducer = combineReducers({missions, dragons, rockets: rocketReducer,});
+const reducer = combineReducers({ missions, dragons, rockets: rocketReducer });
 
 const store = createStore(
   reducer,
