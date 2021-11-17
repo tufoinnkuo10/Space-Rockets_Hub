ff2ef3d2de9d3219407c57f8de83338299b8999c
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-17 15:37:31 +0100

    final api fetch for all

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import missions from './missions/missions';
+import dragons from './dragons/dragons';
 import fetchrocketAPI from '../components/Apirocket';
 import rocketReducer, { getRockets } from './rockets/rockets';
 
 const reducer = combineReducers({
   missions,
+  dragons,
   rockets: rocketReducer,
 });
 
