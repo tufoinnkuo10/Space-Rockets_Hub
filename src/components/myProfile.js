9950509f531915cb0a6b49ee66c2904df56be2f0
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-18 12:23:38 +0100

    render reserve list in profile

--- src/components/myProfile.js
+++ src/components/myProfile.js
@@ -4,28 +4,28 @@ import { useSelector } from 'react-redux';
 const MyProfile = () => {
   const rockets = useSelector((state) => state.rockets);
   return (
-
-  <div className="profile-container">
-    <div className="missions">
-      <p>Missions</p>
-    </div>
-    
-    <div className="rockets">
-        <h2>My Rockets</h2>
-        <div className="display_rocket">
-          {rockets.filter((item) => item.reserved).map((data) => (
-            <li key={data.id} className="display_list_r">
-              {data.rocketName}
-            </li>
-          ))}
+    <>
+      <div className="profile-container">
+        <div className="missions">
+          <p>Missions</p>
         </div>
+        <div className="rockets">
+          <h3 className="rocketprofile"> Rockets</h3>
+          <div className="showrocket">
+            {rockets.filter((item) => item.reserved).map((data) => (
+              <li key={data.id} className="showrockets">
+                {data.rocketName}
+              </li>
+            ))}
+          </div>
 
+        </div>
+        <div className="dragons">
+          <p>Dragons</p>
+        </div>
       </div>
-    </div>
-  )}
-    <div className="dragons">
-      <p>Dragons</p>
-    </div>
-  </div >
-);
+    </>
+  );
+};
+
 export default MyProfile;
