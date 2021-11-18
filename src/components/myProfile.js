b5158504ad0aac3355a18c1afd3e52ca99a5e830
azy64 <allysaidi64@gmail.com>
2021-11-18 09:22:00 +0100

    Update from the dev branch

--- src/components/myProfile.js
+++ src/components/myProfile.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const MyProfile = () => (
-  <div>
-    <div>
+  <div className="container">
+    <div className="missions">
       <p>Missions</p>
     </div>
-    <div>
+    <div className="rockets">
       <p>Rockets</p>
     </div>
-    <div>
+    <div className="dragons">
       <p>Dragons</p>
     </div>
   </div>
