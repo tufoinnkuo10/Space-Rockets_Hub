a4f1f8cbed3dddc7337c785401f8d6c28ac5ccb5
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-18 10:12:38 +0100

    setup profile for rockets

--- src/components/myProfile.js
+++ src/components/myProfile.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div className="container">
+const MyProfile = () => {
+  const rockets = useSelector((state) => state.rockets);
+  return (
+
+  <div className="profile-container">
     <div className="missions">
       <p>Missions</p>
     </div>
+    
     <div className="rockets">
-      <p>Rockets</p>
+        <h2>My Rockets</h2>
+        <div className="display_rocket">
+          {rockets.filter((item) => item.reserved).map((data) => (
+            <li key={data.id} className="display_list_r">
+              {data.rocketName}
+            </li>
+          ))}
+        </div>
+
+      </div>
     </div>
+  )}
     <div className="dragons">
       <p>Dragons</p>
     </div>
-  </div>
+  </div >
 );
 export default MyProfile;
